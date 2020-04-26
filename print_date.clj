efac33edd377d25269412dad48eb464cb257e258
Russell Matney <russell.matney@gmail.com>
2020-04-26 16:20:54 -0400

    feat: add date example

---
+++ print_date.clj
@@ -0,0 +1,8 @@
+(ns print-date)
+
+(def now (java.time.ZonedDateTime/now))
+(def LA-timezone (java.time.ZoneId/of "America/Los_Angeles"))
+(def LA-time (.withZoneSameInstant now LA-timezone))
+(def pattern (java.time.format.DateTimeFormatter/ofPattern "HH:mm"))
+(println "time in LA")
+(println (.format LA-time pattern))
